c2a2b09f272eb78a7062cf5ae1468d86eb41697f
Jennie Kim <thejenniekim@gmail.com>
2015-03-16 15:29:42 -0700

    added twin cat

--- catClicker/js/app.js
+++ catClicker/js/app.js
@@ -9,8 +9,14 @@
 // btn.appendChild(clickOnMeow);
 
 // If you're writing Cat Clicker with vanilla JS (no jQuery), you'll be adding the "click" event with elem.addEventListener()
+var catName = "fluffy";
+document.getElementById('cat-name').innerHTML = catName;
 var catImg = document.getElementById('cat-img');
 var catCount = document.getElementById('cat-count');
+var twinCatName = "buffy";
+document.getElementById('twincat-name').innerHTML = twinCatName;
+var twinCatImg = document.getElementById('twincat-img');
+var twinCatCount = document.getElementById('twincat-count');
 
 var counter = 0;
 var catIncrementer = function(){
@@ -18,4 +24,12 @@ var catIncrementer = function(){
 	catCount.innerText = counter;
 };
 
+var twinCounter = 0;
+var twinCatIncrementer = function(){
+	twinCounter++;
+	twinCatCount.innerText = twinCounter;
+};
+
 catImg.addEventListener('click', catIncrementer, false);
+twinCatImg.addEventListener('click', twinCatIncrementer, false);
+
