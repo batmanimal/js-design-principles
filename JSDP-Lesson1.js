5a4d2685f6c4d6201e7e066b36446cd6a1b8a87e
Jennie Kim <thejenniekim@gmail.com>
2015-03-16 13:57:38 -0700

    added catClicker app

---
+++ JSDP-Lesson1.js
@@ -0,0 +1,12 @@
+// cat clicker project 
+// The application should display a picture of a cat and a number of clicks
+// The number of clicks should increment when the cat picture is clicked
+
+var catClicker;
+// 
+
+// If you're writing Cat Clicker with vanilla JS (no jQuery), you'll be adding the "click" event with elem.addEventListener()
+var elem = document.getElementById('my-elem');
+elem.addEventListener('click', function(){
+  //the element has been clicked... do stuff here
+}, false);
