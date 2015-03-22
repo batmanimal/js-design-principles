2d2fbc68e055ac9097509e3f72938792cd8581b4
thejenniekim <jk@udacity.com>
2015-03-22 10:50:45 -0700

    removed commented out code

--- catClicker/js/app.js
+++ catClicker/js/app.js
@@ -1,13 +1,6 @@
 // cat clicker project 
 // The application should display a picture of a cat and a number of clicks
 // The number of clicks should increment when the cat picture is clicked
-
-// var catClicker;
-// Create a button
-// var btn = document.createElement("button");
-// var clickOnMeow = document.createTextNode("Click on MEOW");
-// btn.appendChild(clickOnMeow);
-
 // If you're writing Cat Clicker with vanilla JS (no jQuery), you'll be adding the "click" event with elem.addEventListener()
 var catName = "fluffy";
 document.getElementById('cat-name').innerHTML = catName;
